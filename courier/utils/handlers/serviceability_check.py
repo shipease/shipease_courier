d93748adb00074a8c639630aa06728f249f6561c
Shipease-Technology <dhananjay.bhadauria@shipease.in>
2024-02-26 17:08:53 +0530

    courier serviceability done

--- courier/utils/handlers/serviceability_check.py
+++ courier/utils/handlers/serviceability_check.py
@@ -3,6 +3,10 @@ from master_app.stub_models import Partner
 
 
 class ServiceabilityCheckHandler:
+    """
+    FM >> First Mile, pickup pincode, partner who can pick from pincode
+    LM >> Last Mile, delivery pincode, partners who deliver to this pincode
+    """
 
     def __init__(self, pin_code) -> None:
         self.pin_code = pin_code
@@ -33,3 +37,21 @@ class ServiceabilityCheckHandler:
             response.append(partner_obj)
 
         return response
+
+    def check_pair_serviceability(self, pickup_pincode, delivery_pincode):
+        fm_partners = ServiceablePincodeFM.objects.values_list(
+            'partner_id', flat=True).filter(pincode=pickup_pincode)
+
+        lm_partners = ServiceablePincode.objects.values_list(
+            'partner_id', flat=True).filter(pincode=delivery_pincode)
+
+        common_partners = list(filter(lambda x: x in fm_partners, lm_partners))
+
+        response = []
+        for partner_id in common_partners:
+            partner_obj = Partner.objects.using('user_db').values('id',
+                                                                  'title', 'mps_enabled', 'reverse_enabled', 'weight_initial', 'extra_limit').get(id=partner_id)
+            response.append(partner_obj)
+
+        return response
+
--- courier/views/seller_views/download_service_pins.py
+++ courier/views/seller_views/download_service_pins.py
@@ -15,7 +15,8 @@ class ServiceablePinDownloadApiView(APIView):
             1. partner_ids=1,2
         """
         response = []
-        for p_id in request.query_params.get('partner_ids'):
+        partsner_ids = request.query_params.get('partner_ids').split(',')
+        for p_id in partsner_ids:
             partner_name = ServiceablePincode.partner_name_by_id(p_id)
             pins_list = ServiceablePincode.service_pin_list(partner_id=p_id)
             result = {"partner_name": partner_name, "pin_list": pins_list}
@@ -31,9 +32,22 @@ class CheckServiceabilityApiView(APIView):
         pincode = request.query_params['pincode']
 
         if pincode_type.strip().lower() == "lm":
-            result = ServiceabilityCheckHandler(pin_code=pincode).check_lm_serviceability()
+            result = ServiceabilityCheckHandler(
+                pin_code=pincode).check_lm_serviceability()
 
         elif pincode_type.strip().lower() == "fm":
-            result = ServiceabilityCheckHandler(pin_code=pincode).check_fm_serviceability()
+            result = ServiceabilityCheckHandler(
+                pin_code=pincode).check_fm_serviceability()
 
+        return Response(result, status=200)
+
+
+class CheckPairServiceabilityApiView(APIView):
+    def get(self, request, *args, **kwargs):
+
+        pickup_pincode = request.query_params.get('pickup_pincode')
+        delivery_pincode = request.query_params.get('delivery_pincode')
+
+        result = ServiceabilityCheckHandler(pin_code=None).check_pair_serviceability(
+            pickup_pincode=pickup_pincode, delivery_pincode=delivery_pincode)
         return Response(result, status=200)
