28e73988c60e0ddb9c2a6974714d1c04ff5d893a
Shipease-Technology <dhananjay.bhadauria@shipease.in>
2024-02-26 17:50:25 +0530

    allowed host done

--- shipease_courier/settings.py
+++ shipease_courier/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-7o$#*@_!rz#dau_f-4yr-3q*sm%qiro#k_cdkhbergcz02!n=0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*",]
 
 
 # Application definition
