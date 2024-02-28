9aa1cc10b72d76c0c81295c06c1e88e78c8cf4d4
Shipease Technologies <99715610+shipease@users.noreply.github.com>
2024-02-28 17:46:01 +0530

    Delhivery AWB Number Table Added

--- courier/models/awb_number.py
+++ courier/models/awb_number.py
@@ -16,6 +16,21 @@ class EkartAWBNumber(TimeStampedModel):
         verbose_name_plural = 'Ekart AWB Numbers'
 
 
+class DelhiveryAWBNumber(TimeStampedModel):
+
+    courier_partner = models.CharField(max_length=30)
+    used = models.BooleanField(default=False, null=True)
+    awb_number = models.CharField(max_length=30)
+    seller_id = models.BigIntegerField(null=True, blank=True,)
+    used_datetime = models.DateTimeField(null=True, blank=True)
+    seller_type = models.CharField(max_length=30, null=True, blank=True, default='SE')
+
+    class Meta:
+        db_table = 'delhivery_awb_number'
+        verbose_name = 'Delhivery AWB Number'
+        verbose_name_plural = 'Delhivery AWB Numbers'
+
+
 class ShadowfaxAWBNumber(TimeStampedModel):
     awb_number = models.CharField(max_length=30)
     flow = models.CharField(max_length=20, default='forward')
